f141a932d386e5c2dce30fe78dec3f10d16b5735
GergoN02 <nemethgergo02@gmail.com>
2021-11-23 03:47:03 +0000

    implemented voting logic

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Int,
   Mutation,
+  ObjectType,
   Query,
   Resolver,
   Root,
@@ -15,6 +16,7 @@ import {
 import { MyContext } from "src/types";
 import { isAuth } from "../middleware/isAuth";
 import { getConnection } from "typeorm";
+import { Upvote } from "../entities/Upvote";
 
 @InputType()
 class PostInput {
@@ -24,39 +26,114 @@ class PostInput {
   text: string;
 }
 
+@ObjectType()
+class PaginatedPosts {
+  @Field(() => [Post])
+  posts: Post[];
+  @Field()
+  hasMore: boolean;
+}
+
 @Resolver(Post)
 export class PostResolver {
 
-    @FieldResolver(() => String)
-    textSnippet(
-      @Root() root: Post
-    ) {
-      return root.text.slice(0, 50);
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 50);
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async vote(
+    @Arg("postId", () => Int) postId: number,
+    @Arg("value", () => Int) value: number,
+    @Ctx() { req }: MyContext
+  ) {
+    const isUpvote = value !== -1;
+    const insertValue = isUpvote ? 1 : -1;
+    const { userId } = req.session;
+
+    const upvote = await Upvote.findOne({where: {postId, userId}})
+
+    //Changing vote on post
+    if(upvote && upvote.value !== insertValue) {
+
+      await getConnection().transaction(async (tm) => {
+        await tm.query(`
+        update upvote
+        set value = $1
+        where "postId" = $2 and "userId" = $3
+        `, [insertValue, postId, userId]);
+
+        await tm.query(`
+        update post
+        set points = points + $1
+        where id = $2
+        `, [2 * insertValue, postId]);
+
+      })
+
+    } else if (!upvote) {
+      //Not voted on post before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+        `insert into upvote ("userId", "postId", value)
+        values ($1, $2, $3)        
+        `, [userId, postId, insertValue]);
+      
+        await tm.query(`
+          update post
+          set points = points + $1
+          where id = $2
+        `, [insertValue, postId]);
+      })
     }
 
+    return true;
+  }
 
-
-
-  @Query(() => [Post]) //GraphQL type
+  @Query(() => PaginatedPosts) //GraphQL type
   async posts(
     @Arg("limit", () => Int) limit: number,
-    @Arg("cursor", () => String, { nullable: true }) cursor: string
-  ): Promise<Post[]> {
-    const realLimit = Math.min(50, limit);
+    @Arg("cursor", () => String, { nullable: true }) cursor: string,
+    @Ctx() {req}: MyContext
+  ): Promise<PaginatedPosts> {
+    const postDisplayLimit = Math.min(50, limit);
+    const paginateFetchLimit = postDisplayLimit + 1; // +1 for checking if there are more posts available, only return realLimit number
 
-    const postQuery = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("p")
-      .orderBy('"createdAt"', "DESC")
-      .take(realLimit);
+    const replacements: any[] = [paginateFetchLimit, req.session.userId];
 
     if (cursor) {
-      postQuery.where('"createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor)),
-      });
+      replacements.push(new Date(parseInt(cursor)));
     }
 
-    return postQuery.getMany();
+    const posts = await getConnection().query(
+      `
+      select p.*,
+      json_build_object(
+        'id', u.id,
+        'username', u.username,
+        'email', u.email,
+        'createdAt', u."createdAt",
+        'updatedAt', u."updatedAt"
+        ) "postCreator",
+        ${
+          req.session.userId ? '(select value from upvote where "userId" = $2 and "postId" = p.id) "voteStatus"' : 'null as "voteStatus"'}
+      from post p
+      inner join public.user u on u.id = p."postCreatorId"
+      ${cursor ? `where p."createdAt" < $3` : ""}
+      order by p."createdAt" DESC
+      limit $1
+      `,
+      replacements
+    );
+
+    console.log("posts: ", posts);
+
+    return {
+      posts: posts.slice(0, postDisplayLimit),
+      hasMore: posts.length === paginateFetchLimit,
+    };
   }
 
   @Query(() => Post, { nullable: true }) //GraphQL type or nullable
